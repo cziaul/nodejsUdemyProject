a5188ce780a3a122a76aaad3d2e408114cf41d17
Ziaul Chowdhury <cziaul@gmail.com>
2016-07-05 19:36:56 -0500

    added new stuff

---
+++ l32_objectProperties/app.js
@@ -0,0 +1,28 @@
+//object properties and methods
+
+var obj = {
+    greet:'Hello'
+}
+
+console.log(obj.greet);
+console.log(obj['greet']);
+
+// I can have a variable set to string and use that variable to access the property of object. 
+var prop = 'greet'
+console.log(obj[prop])
+
+//function and arrays
+
+var arr = []
+
+arr.push(function(){
+    console.log('Hello World 1');
+});
+arr.push(function(){
+    console.log('Hello World 2');
+});
+
+arr.push(function(){
+    console.log('Hello World 3');
+});
+
