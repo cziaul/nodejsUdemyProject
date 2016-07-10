bf9779d2abce978882487a37be849dac177fccbb
Ziaul Chowdhury <cziaul@gmail.com>
2016-07-09 21:10:30 -0500

    upto end of Lecture 77

--- l69_NPMinit_nodemon_packjson/app.js
+++ l69_NPMinit_nodemon_packjson/app.js
@@ -1,9 +1,15 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 
 //adding port from env.
 
 var port = process.env.PORT || 3000;
+
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+//for json
+var jsonParser = bodyParser.json();
 
 //Adding middleware (if url says /assets, it will return css under /public)
 app.use('/assets', express.static(__dirname+'/public'));
@@ -24,10 +30,28 @@ app.get('/', function(req, res){
     //res.send('<html><head><link href=assets/style.css type=text/css rel=stylesheet/></head><body><h1>Hello World!</h1></body></html>');
 });
 
+//Handling query String
+
 app.get('/person/:id', function(req, res){
-    res.render('person', { ID : req.params.id });
+    res.render('person', { ID : req.params.id, Qstr:req.query.qstr });
     //res.send('<html><head></head><body><h1>Person: '+req.params.id+ '</h1></body></html>');
 });
+
+//Handling Post
+app.post('/person', urlencodedParser, function(req, res){
+    res.send('Thank You');
+    console.log(req.body.firstname);
+    console.log(req.body.lastname);
+});
+
+//Handling JSON Data
+
+app.post('/personjson', jsonParser, function(req, res){
+    res.send('Thank You for the JSON data!');
+    console.log(req.body.firstname);
+    console.log(req.body.lastname);
+});
+
 
 /*multi params (Not working)
 app.get('/persons/:page/:id', function(req, res){
